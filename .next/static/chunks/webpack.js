4191b90e505d1511a32fb8d6d62d291acf04d0de
Your Name <tonfroy90@gmail.com>
2023-04-11 23:19:01 -0700

    Building Sidebar 4/11

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1bcf7d73f43a5d24"; }
+/******/ 		__webpack_require__.h = function() { return "fd943040872a49b2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/SideNavbar.js
+++ components/SideNavbar.js
@@ -1,16 +1,28 @@
 import React from "react";
-import Image from 'next/image'
+import Image from "next/image";
+import { BsXDiamondFill } from "react-icons/bs";
+import { IconContext } from "react-icons";
 
 export default function SideNavbar() {
+  // let iconStyles = { color: }
   return (
-    <div className="pt-6 w-52 h-screen flex bg-[#141515] ">
+    <div className="flex">
+      <div className="p-5 pt-8 w-52 h-screen bg-[#141515]">
         <div>
-        <Image src={"/PortfolioHS.jpg"} alt="Headshot" width="150" height="150" />
+          <Image
+            src={"/PortfolioHS.jpg"}
+            alt="Headshot"
+            width="150"
+            height="150"
+          />
         </div>
-        
-        
-        
-      
+        <ul>
+          <li className="text-white flex">
+            <BsXDiamondFill className="text-[#5fefd0]" />
+            Home
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
