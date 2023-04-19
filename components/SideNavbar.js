197d9714ce305c1bf582d4765a4e51cc840781c3
Your Name <tonfroy90@gmail.com>
2023-04-18 18:35:21 -0700

    Adding NavLink

--- components/SideNavbar.js
+++ components/SideNavbar.js
@@ -1,9 +1,9 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import Image from "next/image";
 import { sidebarMenu } from "../data/data";
 
 export default function SideNavbar() {
-  // let iconStyles = { color: }
   return (
     <div className="flex">
       <div className="p-5 pt-8 w-52 h-screen bg-[#141515]">
@@ -15,6 +15,12 @@ export default function SideNavbar() {
             height="150"
           />
         </div>
+        <ul className="side-links">
+          {sidebarMenu.map((link, index) => {
+            const { text, url, icon } = link;
+            return <li key={index}></li>;
+          })}
+        </ul>
         <ul>
           <li className="text-white flex">
             <BsXDiamondFill className="text-[#5fefd0]" />
