c9c71c0f883c9eb264c9de33488c0cbfc3033e88
Your Name <tonfroy90@gmail.com>
2023-05-29 21:36:14 -0700

    Reconfiguring next config file

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
  * @type {import('next').NextConfig}
  */
 
-require('dotenv').config();
+require("dotenv").config();
 
 const nextConfig = {
   output: "export",
@@ -11,10 +11,12 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig, {
-  env: {
-    SERVICE_ID: process.env.SERVICE_ID,
-    TEMPLATE_ID: process.env.TEMPLATE_ID,
-    PUBLIC_KEY: process.env.PUBLIC_KEY,
-  },
-}; 
+module.exports = nextConfig;
+
+// , {
+//   env: {
+//     SERVICE_ID: process.env.SERVICE_ID,
+//     TEMPLATE_ID: process.env.TEMPLATE_ID,
+//     PUBLIC_KEY: process.env.PUBLIC_KEY,
+//   },
+// };
